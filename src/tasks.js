d970d57eb17114ef5fd508048fca829a40f5a673
cgHome <cghome@cflat-inc.org>
2021-02-16 11:44:51 +0100

    Copy templates fixed

--- src/tasks.js
+++ src/tasks.js
@@ -73,13 +73,13 @@ function copyAppTemplates() {
 	try {
 		logSuccess("Copy templates...");
 		// Copy file
-		_copyFiles("files/.gitignore", "./.gitignore", false);
-		_copyFiles("files/.eslintrc.json", "./.eslintrc.json", false);
-		_copyFiles("files/.npm-init.js", "./.npm-init.js");
+		_copyFiles("files/gitignore", "./.gitignore", false);
+		_copyFiles("files/eslintrc.json", "./.eslintrc.json", false);
+		_copyFiles("files/npm-init.js", "./.npm-init.js");
 		// Copy directory
-		_copyFiles(".vscode", ".vscode", false);
-		//_copyFiles(".github", ".github", false);
-		_copyFiles(".devcontainer", ".devcontainer");
+		_copyFiles("vscode", ".vscode", false);
+		//_copyFiles("github", ".github", false);
+		_copyFiles("devcontainer", ".devcontainer");
 		logSuccess("Templates copied");
 	} catch (err) {
 		logError(err);
